aa7475a09929ba7054b09a72baffd97eea994988
Goi Garg <goigarg1@gmail.com>
2021-08-28 02:55:02 +0530

    Update reverseArr.php

--- reverseArr.php
+++ reverseArr.php
@@ -1,9 +1,23 @@
 <?php
 
 $arr = [1,2,3,4,5,6];
-$start = $arr[0];
-$end = $arr[count($arr)-1];
 
-while($start<$end) {
+$end = count($arr) - 1;
 
+
+
+for($i = 0; $i < count($arr); $i++) {
+
+    if($i > $end-$i) {
+        break;
+    }
+    
+    $temp = $arr[$i];
+    $arr[$i] = $arr[$end-$i];
+    $arr[$end-$i] = $temp;
+
+}
+
+foreach($arr as $item) {
+    echo $item . ' ';
 }
